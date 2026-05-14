70f8f18d5a8a01945e7937a823ad0b5dbf7895fc
Carlos Costoso <carlos.costoso@outlook.com>
2026-05-14 14:57:15 -0400

    Adds new products and formatting util

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/lib/mocks";
+import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
 
 export function ProductCard({ product }: { product: Product }) {
@@ -9,11 +10,12 @@ export function ProductCard({ product }: { product: Product }) {
                     src={product.image}
                     alt={product.name}
                     className="object-cover"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     fill
                 />
             </div>
             <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-            <p className="text-gray-600 mb-4">${product.price.toFixed(2)}</p>
+            <p className="text-gray-600 mb-4">{formatPrice(product.price)}</p>
             <p className="text-gray-600 mb-4">{product.description}</p>
         </div>
     );
