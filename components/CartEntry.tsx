47e43976ec331f9989d417b997eb2b23f80b059d
Carlos Costoso <carlos.costoso@outlook.com>
2026-06-01 12:52:51 -0400

    Fixes UI issues with right button on cart entry component

--- components/CartEntry.tsx
+++ components/CartEntry.tsx
@@ -12,7 +12,7 @@ export default function CartEntry({ cartItem }: CartEntryProps) {
     return (
         <li className="border-b border-muted flex py-4 justify-between">
             <div className="flex space-x-4">
-                <div className="overflow-hidden rounded-md border border-muted w-24 h-24">
+                <div className="overflow-hidden rounded-md border border-muted w-20 h-20">
                     {cartItem.product.image && (
                         <Image
                             className="h-full w-full object-cover"
@@ -24,13 +24,11 @@ export default function CartEntry({ cartItem }: CartEntryProps) {
                     )}
                 </div>
                 <div className="flex flex-col">
-                    <h2 className="text-lg font-medium">
-                        {cartItem.product.name}
-                    </h2>
+                    <h2 className="font-medium">{cartItem.product.name}</h2>
                 </div>
             </div>
 
-            <div className="flex flex-col items-end">
+            <div className="flex flex-col justify-between items-end gap-2">
                 <p className="font-medium">
                     Price: {formatPrice(cartItem.product.price)}
                 </p>
@@ -47,7 +45,7 @@ export default function CartEntry({ cartItem }: CartEntryProps) {
                     <Button
                         variant="ghost"
                         size="icon"
-                        className="rounded-l-full"
+                        className="rounded-r-full"
                     >
                         <Plus size={16} />
                     </Button>
