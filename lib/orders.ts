62fb610172317dc88443bc147720f2f633cb3b2d
Carlos Costoso <carlos.costoso@outlook.com>
2026-06-01 18:00:45 -0400

    Implements createCheckoutSession method

--- lib/orders.ts
+++ lib/orders.ts
@@ -1,22 +1,68 @@
 "use server";
 
+import { cookies } from "next/dist/server/request/cookies";
 import { getCheckoutCart } from "./actions/cart-actions";
+import { prisma } from "./prisma";
+import { Prisma } from "@/generated/prisma/client";
 
-export async function createOrder() {
+export async function processCheckout() {
     const cart = await getCheckoutCart();
 
     if (!cart || cart.items.length === 0) {
         throw new Error("Cart is empty");
     }
 
-    // Here you would typically create an order in your database and return the order details
-    // For demonstration purposes, we'll just return a mock order object
+    try {
+        const order = await prisma.$transaction(async (tx) => {
+            const total = cart.subtotal;
 
-    const order = {
-        id: "order_123",
-        total: cart.items.reduce((sum, item) => sum + item.product.price * item.quantity, 0),
-        items: cart.items,
-    };
+            // Create a new order in the database
+            const newOrder = await tx.order.create({
+                data: { total }
+            });
 
-    return order;
+            // Map cart items to order items
+            const orderItems: Prisma.OrderItemCreateManyInput[] = cart.items.map((item) => ({
+                orderId: newOrder.id,
+                productId: item.product.id,
+                quantity: item.quantity,
+                price: item.product.price,
+            }));
+
+            // Create order items in the database
+            await tx.orderItem.createMany({
+                data: orderItems,
+            });
+
+            // Clear the cart
+            await tx.cartItem.deleteMany({
+                where: { cartId: cart.id },
+            });
+
+            // Delete the cart itself
+            await tx.cart.delete({
+                where: { id: cart.id },
+            });
+
+            return newOrder;
+        });
+
+        (await cookies()).delete("cartId");
+
+        return order;
+    } catch (error) {
+        console.error("Error creating order:", error);
+        throw new Error("Failed to create order");
+    }
+
+    /*
+    TODO:
+    1. Calculate total price
+    2. Create order in the database
+    3. Create order items in the database
+    4. Clear the cart
+    5. Revalidate cache
+    6. Return order details
+
+    */
 }
--- lib/types.ts
+++ lib/types.ts
@@ -1,15 +1,5 @@
 import { Prisma } from "@/generated/prisma/client";
 
-export type CheckoutCart = ProductCart & {
-    size: number;
-    subtotal: number;
-};
-
-// Prisma.CartGetPayload is a utility type that generates the TypeScript 
-// type for the result of a Prisma query on the Cart model, including 
-// the specified relations and fields.
-// Ensures that the ProductCart type includes the related items and 
-// their associated products when fetching a cart from the database.
 export type ProductCart = Prisma.CartGetPayload<{
     include: {
         items: {
@@ -20,8 +10,23 @@ export type ProductCart = Prisma.CartGetPayload<{
     };
 }>;
 
+export type CheckoutCart = ProductCart & {
+    size: number;
+    subtotal: number;
+};
+
 export type CartItemWithProduct = Prisma.CartItemGetPayload<{
     include: {
         product: true;
+    };
+}>;
+
+export type OrderWithItemsAndProduct = Prisma.OrderGetPayload<{
+    include: {
+        orderItems: {
+            include: {
+                product: true;
+            };
+        };
     };
 }>;
