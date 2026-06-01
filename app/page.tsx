e0bb424db6a38fa63a217517ba2ecdb866db917d
Carlos Costoso <carlos.costoso@outlook.com>
2026-06-01 16:46:34 -0400

    Adds stripe npm and tests in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { Suspense } from "react";
 import ProductsSkeleton from "../components/skeletons/ProductsSkeleton";
 import Breadcrumbs from "@/components/Breadcrumbs";
 import ProductListData from "@/components/ProductListData";
+import { stripe } from "@/lib/stripe";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 const PAGE_SIZE = 3;
@@ -23,6 +24,8 @@ export default async function HomePage(props: { searchParams: SearchParams }) {
 
     // Calculate total pages based on total products and page size
     const totalPages = Math.ceil(total / PAGE_SIZE);
+
+    console.log(await stripe.events.list({ limit: 1 }));
 
     return (
         <main className="container mx-auto p-4 flex-1">
