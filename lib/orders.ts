ca1a40dc28f8a2c06a78012d9425d4c72296d65d
Carlos Costoso <carlos.costoso@outlook.com>
2026-06-01 18:08:10 -0400

    Adds implementation for updating order upon checkout session creation

--- lib/orders.ts
+++ lib/orders.ts
@@ -4,6 +4,7 @@ import { cookies } from "next/dist/server/request/cookies";
 import { getCheckoutCart } from "./actions/cart-actions";
 import { prisma } from "./prisma";
 import { Prisma } from "@/generated/prisma/client";
+import { createCheckoutSession } from "./stripe";
 
 export async function processCheckout() {
     const cart = await getCheckoutCart();
@@ -47,9 +48,44 @@ export async function processCheckout() {
             return newOrder;
         });
 
+        // 1. Reload full order
+
+        const createdOrder = await prisma.order.findUnique({
+            where: { id: order.id },
+            include: {
+                orderItems: {
+                    include: {
+                        product: true,
+                    },
+                },
+            },
+        });
+
+        // 2. Confirm that the order was created successfully
+        if (!createdOrder) {
+            throw new Error("Order not found");
+        }
+
+        // 3. Create the Stripe session
+        const { sessionId, sessionUrl } = await createCheckoutSession(createdOrder);
+
+        // 4. Return the session URL and handle errors
+        if (!sessionId || !sessionUrl) {
+            throw new Error("Failed to create Stripe checkout session");
+        }
+
+        // 5. Store the session ID in the order and change the order status
+        await prisma.order.update({
+            where: { id: createdOrder.id },
+            data: {
+                stripeSessionId: sessionId,
+                status: "pending",
+            },
+        });
+
         (await cookies()).delete("cartId");
 
-        return order;
+        return createdOrder;
     } catch (error) {
         console.error("Error creating order:", error);
         throw new Error("Failed to create order");
